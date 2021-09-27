65ed74f716266362e4428c7cf86ca0fe8246b3a7
risyad <risyadzyn@gmail.com>
2021-09-27 20:12:28 +0700

    movie container updated

--- src/components/movie-container/MovieContainer.js
+++ src/components/movie-container/MovieContainer.js
@@ -1,6 +1,7 @@
-import './MovieContainer.css'
+import MovieContainerCss from './MovieContainer.module.css'
+// import './MovieContainer.css'
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
 import { MovieDB } from '../../services';
 
 function MovieContainer() {
@@ -18,32 +19,70 @@ function MovieContainer() {
 
   return (
     <>
-      <div className='filterContainer'>
+      <div className={MovieContainerCss.filterContainer}>
         <h3>Browse by category</h3>
-        <div className='filters'>
+        <Router>
+          <ul className={MovieContainerCss.filters}>
+            <li>
+              <NavLink exact to='/' activeClassName={MovieContainerCss.active}>
+                All
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact to='/action' activeClassName={MovieContainerCss.active}>
+                Action
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/adventure' activeClassName={MovieContainerCss.active}>
+                Adventure
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/anime' activeClassName={MovieContainerCss.active}>
+                Anime
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/comedy' activeClassName={MovieContainerCss.active}>
+                Comedy
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/science-fiction' activeClassName={MovieContainerCss.active}>
+                Science Fiction
+              </NavLink>
+            </li>
+          </ul>
+        </Router>
+      </div>
+
+      {/* <div className={MovieContainerCss.filterContainer}>
+        <h3>Browse by category</h3>
+        <div className={MovieContainerCss.filters}>
           {genres.map(genre => (
             <button
               key={genre}
               onClick={() => setActiveFilter(genre)}
-              className={`filters-item ${genre == activeFilter ? 'active' : ''}`}>
+              className={`${MovieContainerCss.filtersItem} ${genre === activeFilter ? MovieContainerCss.active : ''}`}>
               {genre}
             </button>
           ))}
         </div>
-      </div>
+      </div> */}
 
 
-      <div className="field">
+      <div className={MovieContainerCss.movieCollection}>
         {movies.map((movie) => {
           return (
-            <div key={movie._id} className="movieBox">
+            <div key={movie._id} className={MovieContainerCss.movieBox}>
               <img
                 src={`https://image.tmdb.org/t/p/original${movie.poster}`}
                 alt={movie.title}
               />
-              <div className='movieInfo'>
-                <p className='titleInfo'>{movie.title}</p>
-                <p className='genreInfo'>{movie.genres[0]}</p>
+              <div className={MovieContainerCss.movieInfo}>
+                <p className={MovieContainerCss.genreInfo}>{`${movie.genres[0]}, ${movie.genres[1]}`}</p>
+                <p className={MovieContainerCss.titleInfo}>{movie.title}</p>
               </div>
             </div>
           );
